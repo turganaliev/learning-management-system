ee9761c83660669ac092a3d92ec510aa78563c99
turganaliev <izatturganaliev@gmail.com>
2026-03-27 07:41:56 +0600

    Refactor: organize project into controller, service, repository and model packages

--- src/main/java/com/turganaliev/learning_management/controller/GoodByeController.java
+++ src/main/java/com/turganaliev/learning_management/controller/GoodByeController.java
@@ -1,4 +1,4 @@
-package com.turganaliev.learning_management;
+package com.turganaliev.learning_management.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
